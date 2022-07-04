b8e20ae9aacb4f6d7c3b9ce71c6a815054237d71
Jobayerrahman <jobayer.aiub@gmail.com>
2022-07-04 18:00:46 +0600

    Navbar Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,13 +48,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link btn btn-outline-success mx-1" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn btn-outline-secondary mx-1" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
